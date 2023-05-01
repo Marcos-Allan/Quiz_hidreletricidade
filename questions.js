c5e3f59924055f3592649e5b044b6b67537ed3c0
Marcos Allan <106402058+Marcos-Allan@users.noreply.github.com>
2023-04-30 23:40:31 -0300

    Adicionado as perguntas e respostas do Quiz

---
+++ questions.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    question: "Questão 1",
+    answers: [
+      { option: "Verdadeira", correct: true },
+      { option: "Falsa", correct: false },
+      { option: "Falsa", correct: false },
+      ],
+  },
+  {
+    question: "Questão 2",
+    answers: [
+      { option: "Falsa", correct: false },
+      { option: "Verdadeira", correct: true },
+      { option: "Falsa", correct: false },
+      ],
+  },
+  ]
